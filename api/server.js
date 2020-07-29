96a67377ec64c242c0bfa9ad226364d4fc309f19
alex <jesus.galaz@softtek.com>
2020-07-29 17:37:43 -0500

    finish chatApp

--- api/server.js
+++ api/server.js
@@ -21,14 +21,36 @@ const ENV = process.env.NODE_ENV
 const PORT = process.env.PORT
 const server = http.Server(app)
 const io = socketIo(server)
+import util from 'util'
 
 io.on('connection', socket => {
   console.log(chalk.blue(`new connection ${socket.id}`))
+  const rooms = getRooms('connected')
+  console.log({ rooms })
+  io.emit('rooms', rooms)
 
-  socket.on('chatMessage', message => {
-    console.log(chalk.green(`chatMessage - ${socket.id} :`))
-    // send message to all the sockets, except this socket.
-    socket.broadcast.emit('chatMessage', message)
+  socket.on('newRoom', function (room) {
+    console.log(`A new room is created ${room}`)
+    socket.room = room
+    socket.join(room)
+    io.emit('rooms', getRooms('new room'))
+  })
+
+  socket.on('joinRoom', function (room) {
+    console.log(`A new user joined room ${room}`)
+    socket.room = room
+    socket.join(room)
+    io.emit('rooms', getRooms('joined room'))
+  })
+
+  socket.on('chatMessage', function (data) {
+    console.log({ data })
+    io.to(data.room).emit('chatMessage', `${data.name}: ${data.msg}`)
+  })
+
+  socket.on('setUserName', function (name) {
+    console.log(`username set to ${name}(${socket.id})`)
+    socket.username = name
   })
 
   socket.on('disconnect', function () {
@@ -46,3 +68,37 @@ if (ENV === 'development') {
 server.listen(PORT, async () => {
   console.log(chalk.green('server started :'))
 })
+
+function getRooms(msg) {
+  const nsp = io.of('/')
+  const rooms = nsp.adapter.rooms
+  /*Returns data in this form
+  {
+    'roomid1': { 'socketid1', socketid2', ...},
+    ...
+  }
+  */
+  //console.log('getRooms rooms>>' + util.inspect(rooms));
+
+  const list = {}
+
+  for (let roomId in rooms) {
+    const room = rooms[roomId]
+    if (room === undefined) continue
+    const sockets = []
+    let roomName = ''
+    //console.log('getRooms room>>' + util.inspect(room));
+    for (let socketId in room.sockets) {
+      const socket = nsp.connected[socketId]
+      if (socket === undefined || socket.username === undefined || socket.room === undefined) continue
+      //console.log(`getRooms socket(${socketId})>>${socket.username}:${socket.room}`);
+      sockets.push(socket.username)
+      if (roomName == '') roomName = socket.room
+    }
+    if (roomName != '') list[roomName] = sockets
+  }
+
+  console.log(`getRooms: ${msg} >>` + util.inspect(list))
+
+  return list
+}
--- src/index.js
+++ src/index.js
@@ -2,30 +2,60 @@ import './styles.css'
 import $ from 'jquery'
 import io from 'socket.io-client'
 
-const socket = io()
-
-socket.on('connect', connection => {
-  console.log(`client connected ${socket.id}`)
-})
-
-const form = $('form')
-const messageInput = $('#m')
-const messages = $('#messages')
-
 const messageTemplate = ({ id }, message) => `<li>${id} : ${message}</li>`
 
-form.submit(function (e) {
-  e.preventDefault()
-  const message = messageInput.val()
-  console.log(`message send it : ${message}`)
+const socket = io()
+let room, name
 
-  socket.emit('chatMessage', message)
-  messageInput.val('')
-
+$('#name-form').submit(function () {
+  name = $('#m1').val()
+  socket.emit('setUserName', name)
+  $('.name').hide()
+  $('.rooms').show()
+  $('#room').text(`Rooms - (${name})`)
   return false
 })
 
-socket.on('chatMessage', message => {
-  console.log(`message received ${message}`)
-  messages.append(messageTemplate(socket, message))
+$('#room-form').submit(function () {
+  room = $('#m2').val()
+  socket.emit('newRoom', room)
+  $('#m2').val('')
+  $('.rooms').hide()
+  $('.msgs').show()
+  $('#room').text(room)
+  return false
+})
+
+$('#msg-form').submit(function () {
+  socket.emit('chatMessage', { name, room, msg: $('#m3').val() })
+  $('#m3').val('')
+  return false
+})
+
+socket.on('chatMessage', function (msg) {
+  console.log(msg)
+  $('#messages').append($('<li>').text(msg))
+})
+
+socket.on('rooms', function (rooms) {
+  try {
+    $('#room-list').html('')
+    if (rooms !== undefined && Object.getOwnPropertyNames(rooms).length > 0) {
+      for (let room in rooms) {
+        const sockets = rooms[room]
+        let txt = `${room} - ` + sockets.join(', ')
+        let li = $('#room-list').append($('<li>').append($(`<a name="${room}">`).text(txt)))
+      }
+    }
+    $('#room-list a').click(function () {
+      console.log(`Room ${this.name} clicked.`)
+      socket.emit('joinRoom', this.name)
+      room = this.name
+      $('.rooms').hide()
+      $('.msgs').show()
+      $('#room').text(room)
+    })
+  } catch (e) {
+    console.log('There was an error getting the room list')
+  }
 })
